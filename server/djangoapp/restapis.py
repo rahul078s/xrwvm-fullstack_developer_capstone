c163d4388af555ad5d90fbbf92095b4b4635f744
Rahul <rahulsandhu527@gmail.com>
2026-03-29 07:16:38 -0400

    Showin reviews and dealer details or reviews page

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,48 +1,58 @@
-# Uncomment the imports below before you add the function code
 import requests
 import os
+from pathlib import Path
 from dotenv import load_dotenv
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
 
 load_dotenv()
 
 backend_url = os.getenv(
     'backend_url', default="http://localhost:3030")
-sentiment_analyzer_url = os.getenv(
-    'sentiment_analyzer_url',
-    default="http://localhost:5050/")
+
+# Make the bundled VADER lexicon available to NLTK.
+NLTK_DATA_DIR = Path(__file__).resolve().parent / "microservices"
+if str(NLTK_DATA_DIR) not in nltk.data.path:
+    nltk.data.path.append(str(NLTK_DATA_DIR))
+
+sia = SentimentIntensityAnalyzer()
+
 
 def get_request(endpoint, **kwargs):
     params = ""
     if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
+        for key, value in kwargs.items():
+            params = params + key + "=" + value + "&"
 
-    request_url = backend_url+endpoint+"?"+params
+    request_url = backend_url + endpoint + "?" + params
 
     print("GET from {} ".format(request_url))
     try:
-        # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
     except:
-        # If any error occurs
         print("Network exception occurred")
 
 
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url+"analyze/"+text
-    try:
-        # Call get method of requests library with URL and parameters
-        response = requests.get(request_url)
-        return response.json()
-    except Exception as err:
-        print(f"Unexpected {err=}, {type(err)=}")
-        print("Network exception occurred")
+    scores = sia.polarity_scores(text)
+    pos = float(scores['pos'])
+    neg = float(scores['neg'])
+    neu = float(scores['neu'])
+
+    sentiment = "positive"
+    if neg > pos and neg > neu:
+        sentiment = "negative"
+    elif neu > neg and neu > pos:
+        sentiment = "neutral"
+
+    return {"sentiment": sentiment}
+
 
 def post_review(data_dict):
-    request_url = backend_url+"/insert_review"
+    request_url = backend_url + "/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
     except:
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -93,7 +93,7 @@ def get_dealer_reviews(request, dealer_id):
     # if dealer id has been provided
     if(dealer_id):
         endpoint = "/fetchReviews/dealer/"+str(dealer_id)
-        reviews = get_request(endpoint)
+        reviews = get_request(endpoint) or []
         for review_detail in reviews:
             response = analyze_review_sentiments(review_detail['review'])
             print(response)
